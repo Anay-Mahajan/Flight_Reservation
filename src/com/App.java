b4dcb879b692ad2dd879c2fadd1d9f3e6bb70d88
Anay Mahajan <anaymahajan@Anays-MacBook-Air.local>
2025-04-02 20:42:52 +0530

    3rd commit

--- src/com/App.java
+++ src/com/App.java
@@ -1,101 +1,18 @@
+package com;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.Scanner;
-import com.Customer;
-import com.Insufficient_Seats;
 import com.airline.Admin;
 import com.airline.Flight;
 public class App {
-    static ArrayList<Flight>Flight_List=new ArrayList<>();
-    static ArrayList<Admin>Admin_List=new ArrayList<>();
-    static ArrayList<Customer>User_List=new ArrayList<>();
-    public static void User_Menu(Customer U){
-        int choice;
-        @SuppressWarnings("resource")
-        Scanner scanner=new Scanner(System.in);
-        do {
-            System.out.println("\n===== Flight Reservation System =====");
-            System.out.println("1. Book Ticket");
-            System.out.println("2. Log Out");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter Flight Number: ");
-                    int flightNumber = scanner.nextInt();
-                    System.out.print("Enter Number of Seats: ");
-                    int numberOfSeats = scanner.nextInt();
-                    boolean flag=false;
-                    Iterator<Flight> it = Flight_List.iterator();
-                    while (it.hasNext()) {
-                        Flight F = it.next();
-                        it.remove();
-                        if (F.Flight_No == flightNumber) {
-                            flag = true;
-                            try {
-                                Flight_List.add(U.Book_Ticket(F, numberOfSeats)); 
-                            } catch (Insufficient_Seats e) {
-                                System.out.println(e.getMessage());
-                                Flight_List.add(F);
-                            }
-                            updateFlightDataToFile();
-                            break;
-                        }
-                    }
-                    if(!flag){
-                        System.out.println("Enter a valid Flight Number");
-                    }
-                    break;
-                case 2:
-                    System.out.println("Logging out... Thank you for using our system!");
-                    break;
-                default:
-                    System.out.println("Invalid choice! Please enter 1 or 2.");
-            }
-        } while (choice != 2);
-
-    }
-    public static void Admin_Menu(Admin A){
-        int choice;
-        @SuppressWarnings("resource")
-        Scanner scanner=new Scanner(System.in);
-        do {
-            System.out.println("\n===== Flight Reservation System =====");
-            System.out.println("1. Add Flight ");
-            System.out.println("2. Log Out");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter Flight No: ");
-                    int flightNo = scanner.nextInt();
-                    scanner.nextLine(); 
-                    System.out.print("Enter Source: ");
-                    String source = scanner.nextLine();
-                    System.out.print("Enter Destination: ");
-                    String destination = scanner.nextLine();
-                    System.out.print("Enter Cost: ");
-                    int cost = scanner.nextInt();
-                    System.out.print("Enter Number of Seats: ");
-                    int seats = scanner.nextInt();
-                    Flight_List.add(A.Add_Flight(flightNo, source, destination, cost, seats));
-                    updateFlightDataToFile();
-                    break;
-                case 2:
-                    System.out.println("Logging out... Thank you for using our system!");
-                    break;
-                default:
-                    System.out.println("Invalid choice! Please enter 1 or 2.");
-            }
-
-        } while (choice != 2);        
-    }
+    public static ArrayList<Flight>Flight_List=new ArrayList<>();
+    public static ArrayList<Admin>Admin_List=new ArrayList<>();
+    public static ArrayList<Customer>User_List=new ArrayList<>();
     public static void updateFlightDataToFile() {
         String filePath = "flight.txt";
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
@@ -105,6 +22,21 @@ public class App {
                 writer.newLine();
             }
             System.out.println("Flight data updated in Database.");
+        } catch (IOException e) {
+            System.out.println("Error updating flight data: " + e.getMessage());
+        }
+    }
+    public static void updateUserDataToFile() {
+        String filePath = "user_flight.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Customer C : User_List) {
+                writer.write(C.user_id);
+                for(int f: C.Booking_Details.keySet()){
+                    writer.write(" "+f+" "+C.Booking_Details.get(f));
+                }
+                writer.newLine();
+            }
+            System.out.println("User Data Updated into the file.");
         } catch (IOException e) {
             System.out.println("Error updating flight data: " + e.getMessage());
         }
@@ -119,17 +51,8 @@ public class App {
             System.out.println("Error updating user data: " + e.getMessage());
         }
     }
-    public static void main(String[] args) throws Exception {
+    private  static void Load_the_data(){
         String filePath = "flight.txt";
-        File flightFile = new File(filePath);
-        if (!flightFile.exists()) {
-            try {
-                flightFile.createNewFile(); 
-                System.out.println("flight.txt file created.");
-            } catch (IOException e) {
-                System.out.println("Error creating flight.txt: " + e.getMessage());
-            }
-        }
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) { 
@@ -147,15 +70,6 @@ public class App {
             System.out.println("Error reading file: " + e.getMessage());
         }
         String filePath2 = "admin.txt";
-        File adminFile = new File(filePath2);
-        if (!adminFile.exists()) {
-            try {
-                adminFile.createNewFile();
-                System.out.println("admin.txt file created.");
-            } catch (IOException e) {
-                System.out.println("Error creating admin.txt: " + e.getMessage());
-            }
-        }
         try (BufferedReader br2 = new BufferedReader(new FileReader(filePath2))) {
             String line;
             while ((line = br2.readLine()) != null) { 
@@ -166,24 +80,50 @@ public class App {
         catch (IOException | NumberFormatException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
-        String filePath3="user.txt";
-        try (BufferedReader br3 = new BufferedReader(new FileReader(filePath3))) {
+        String filepath4="user_flight.txt";
+        HashMap<String,HashMap<Integer,Integer>>Booking_Database =new HashMap<String,HashMap<Integer,Integer>>();
+        try (BufferedReader br4=new BufferedReader(new FileReader(filepath4))){
             String line;
-            while ((line = br3.readLine()) != null) { 
-                String[] data = line.split(" ");
-                User_List.add(new Customer(data[0], data[1]));
+            while((line =br4.readLine())!=null){
+                String[] data =line.split(" ");
+                int n=data.length;
+                HashMap<Integer,Integer>temp=new HashMap<Integer,Integer>();
+                for(int i=1;i<n;i+=2){
+                    temp.put(Integer.parseInt(data[i]),Integer.parseInt(data[i+1]));
+                }
+                Booking_Database.put(data[0], temp);
             }
         } 
         catch (IOException | NumberFormatException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
+        String filePath3="user.txt";
+        try (BufferedReader br3 = new BufferedReader(new FileReader(filePath3))) {
+            String line;
+            while ((line = br3.readLine()) != null) { 
+                String[] data = line.split(" ");
+                Customer C=new Customer(data[0], data[1]);
+                if(Booking_Database.containsKey(C.user_id)){
+                    for(int flight_no:Booking_Database.get(C.user_id).keySet()){
+                        C.Booking_Details.put(flight_no,Booking_Database.get(C.user_id).get(flight_no));
+                    }
+                }
+                User_List.add(C);
+            }
+        } 
+        catch (IOException | NumberFormatException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        App.Load_the_data();
         Scanner scanner = new Scanner(System.in);
         int choice;
         while (true) { 
             System.out.println("\n===== Flight Reservation System =====");
             System.out.println("1. User Login ");
             System.out.println("2. Admin Login ");
-            System.out.println("3.New User Registeration");
+            System.out.println("3.New User Registration");
             System.out.println("4. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
@@ -198,7 +138,7 @@ public class App {
                     boolean flag=false;
                     for(Customer U :User_List){
                         if(U.user_id.equals(id) && U.password.equals(pass)){
-                            User_Menu(U);
+                            U.User_Menu();
                             flag=true;
                         }
                     }
@@ -217,7 +157,7 @@ public class App {
                         boolean flag1=false;
                         for(Admin A :Admin_List){
                             if(A.Admin_ID.equals(aid) && A.password.equals(apass)){
-                                Admin_Menu(A);
+                                A.Admin_Menu();
                                 flag1=true;
                             }
                         }
@@ -226,7 +166,7 @@ public class App {
                         }
                     break;
                 case 3:
-                    System.out.println("Enter User Name to be set : ");
+                    System.out.println("Enter a User_ID : ");
                     scanner.nextLine();
                     String uid=scanner.nextLine();
                     System.out.println("Enter Password to be set : ");
--- src/com/airline/Admin.java
+++ src/com/airline/Admin.java
@@ -1,4 +1,7 @@
 package com.airline;
+import java.util.Scanner;
+import com.App;
+import com.Customer;
 public class Admin {
     public String Admin_ID;
     public String password;
@@ -6,8 +9,54 @@ public class Admin {
         Admin_ID = admin_ID;
         this.password = password;
     }
-    public Flight Add_Flight(int flight_No, String source, String destination, int cost, int seats_Available)  {
-        Flight F=new Flight(flight_No, source, destination, cost, seats_Available);
-        return F;
+    private void Add_New_Flight(){
+        @SuppressWarnings("resource")
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter Flight No: ");
+        int flightNo = scanner.nextInt();
+        scanner.nextLine(); 
+        System.out.print("Enter Source: ");
+        String source = scanner.nextLine();
+        System.out.print("Enter Destination: ");
+        String destination = scanner.nextLine();                
+        System.out.print("Enter Cost: ");
+        int cost = scanner.nextInt();
+        System.out.print("Enter Number of Seats: ");
+        int seats = scanner.nextInt();
+        App.Flight_List.add(new Flight(flightNo, source, destination, cost, seats));
+        App.updateFlightDataToFile();       
+    }
+    public void Admin_Menu(){
+        int choice;
+        @SuppressWarnings("resource")
+        Scanner scanner=new Scanner(System.in);
+        do {
+            System.out.println("\n===== Flight Reservation System =====");
+            System.out.println("1. Add Flight ");
+            System.out.println("2. Log Out");
+            System.out.println("3.Customer Details");
+            System.out.println("4.Flight Details");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    Add_New_Flight();
+                    break;
+                case 2:
+                    System.out.println("Logging out... Thank you for using our system!");
+                    break;
+                case 3:
+                    for(Customer C : App.User_List){
+                        C.Customer_details();
+                    }
+                case 4:
+                    for(Flight f:App.Flight_List){
+                        f.Display();
+                    }
+                default:
+                    System.out.println("Invalid choice! Please enter 1 or 2.");
+            }
+
+        } while (choice != 2);       
     }
 }
